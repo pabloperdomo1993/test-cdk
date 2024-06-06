70a87a232cfe5540efef85b6cec6abc4c576d6ba
Pablo Perdomo <pabloalejandroperdomo@gmail.com>
2024-06-06 14:21:06 -0500

    SE-SQS to save searchs

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -12,9 +12,19 @@ export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queueUserCreate = new sqs.Queue(this, 'user_distribute', {
+    const queueUserDistribute = new sqs.Queue(this, 'user_distribute', {
       visibilityTimeout: cdk.Duration.seconds(300),
       queueName: 'user_distribute'
+    });
+
+    const queueUserUpdate = new sqs.Queue(this, 'user_update', {
+      visibilityTimeout: cdk.Duration.seconds(300),
+      queueName: 'user_update'
+    });
+
+    const queueUserSave = new sqs.Queue(this, 'user_save', {
+      visibilityTimeout: cdk.Duration.seconds(300),
+      queueName: 'user_save'
     });
 
     const lambdaUserUpdate = lambda.Function.fromFunctionArn(
@@ -23,12 +33,22 @@ export class CdkStack extends cdk.Stack {
       'arn:aws:lambda:us-east-2:943766074476:function:back-end-dev-userUpdate'
     );
 
+    const lambdaUserSave = lambda.Function.fromFunctionArn(
+      this,
+      'back-end-dev-userSave',
+      'arn:aws:lambda:us-east-2:943766074476:function:back-end-dev-userSave'
+    );
+
     const lambdaUserDistribute = lambda.Function.fromFunctionArn(
       this,
       'back-end-dev-distributeData',
       'arn:aws:lambda:us-east-2:943766074476:function:back-end-dev-distributeData'
     );
 
-    lambdaUserUpdate.addEventSource(new lambdaEventSources.SqsEventSource(queueUserCreate));
+    lambdaUserDistribute.addEventSource(new lambdaEventSources.SqsEventSource(queueUserDistribute));
+
+    lambdaUserUpdate.addEventSource(new lambdaEventSources.SqsEventSource(queueUserUpdate));
+    
+    lambdaUserSave.addEventSource(new lambdaEventSources.SqsEventSource(queueUserSave));
   }
 }
